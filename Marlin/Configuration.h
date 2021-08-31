9e465b474ccd915855f2c427801b0eedebfd016a
Scott Mikutsky <smikutsky@gmail.com>
2021-08-31 02:14:56 -0400

    fixed it

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -139,7 +139,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define BOARD_BTT_SKR_V2_0_REV_B
+  #define MOTHERBOARD BOARD_BTT_SKR_V2_0_REV_B
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
