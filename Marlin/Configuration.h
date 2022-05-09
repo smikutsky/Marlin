e91809d89abb4c29b0ae29f22a327588cbc8331d
Scott Mikutsky <smikutsky@gmail.com>
2022-05-09 18:44:51 -0400

    Made to work with DGUS Reloaded UI

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1613,7 +1613,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 4
+  #define GRID_MAX_POINTS_X 5
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
@@ -2603,8 +2603,8 @@
 //#define DGUS_LCD_UI_ORIGIN
 //#define DGUS_LCD_UI_FYSETC
 //#define DGUS_LCD_UI_HIPRECY
-#define DGUS_LCD_UI_MKS
-//#define DGUS_LCD_UI_RELOADED
+//#define DGUS_LCD_UI_MKS
+#define DGUS_LCD_UI_RELOADED
 #if ENABLED(DGUS_LCD_UI_MKS)
   #define USE_MKS_GREEN_UI
 #endif
