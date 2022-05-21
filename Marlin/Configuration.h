556546a29707369990b0cf91973700f20249db33
Scott Mikutsky <smikutsky@gmail.com>
2022-05-21 12:50:12 -0400

    Fixed steps per unit after 256 microstepping

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -927,7 +927,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 821.2 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 2560, 2560, 12800, 13139.2 }
 
 /**
  * Default Max Feed Rate (mm/s)
