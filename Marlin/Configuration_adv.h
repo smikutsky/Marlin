9968ebaeac4e31c257b513d05b27eb16ae24e989
Scott Mikutsky <smikutsky@gmail.com>
2023-05-13 23:26:40 -0400

    Oops

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3999,7 +3999,7 @@
  * Potentially useful for emergency stop that allows being resumed.
  * @section interface
  */
-#define FREEZE_FEATURE
+//#define FREEZE_FEATURE
 #if ENABLED(FREEZE_FEATURE)
   //#define FREEZE_PIN 41   // Override the default (KILL) pin here
   #define FREEZE_STATE LOW  // State of pin indicating freeze
